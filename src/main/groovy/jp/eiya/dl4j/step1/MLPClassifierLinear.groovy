2c166eb9dad6811028f8cbe2c72e657efb727513
bitsnaps <bitsnaps@yahoo.fr>
2018-09-18 23:01:20 +0100

    code update
    Update class to the new dl4j api.

--- src/main/groovy/jp/eiya/dl4j/step1/MLPClassifierLinear.groovy
+++ src/main/groovy/jp/eiya/dl4j/step1/MLPClassifierLinear.groovy
@@ -11,11 +11,12 @@ import org.deeplearning4j.eval.*
 
 import org.deeplearning4j.optimize.listeners.*
 
-import org.datavec.api.records.reader.RecordReader
+//import org.datavec.api.records.reader.RecordReader
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader
 import org.datavec.api.split.FileSplit
 
 import org.nd4j.linalg.activations.Activation
+import org.nd4j.linalg.learning.config.Nesterovs
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
 
 /**
@@ -23,78 +24,81 @@ import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction
  * @see https://github.com/deeplearning4j/dl4j-examples/blob/master/dl4j-examples/src/main/java/org/deeplearning4j/examples/feedforward/classification/MLPClassifierLinear.java
  */
 class MLPClassifierLinear {
-  final static layer = [
-     input  : 2,
-     hidden : 20,
-     output : 2
-  ]
 
-  static class DenseLayerBuilder extends DenseLayer.Builder{} 
-  static class OutputLayerBuilder extends OutputLayer.Builder{
-    OutputLayerBuilder(org.nd4j.linalg.lossfunctions.ILossFunction lossFunction) {super(lossFunction)}
-    OutputLayerBuilder(org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction lossFunction) {super(lossFunction)}
-  } 
+      final static layer = [
+              input  : 2,
+              hidden : 20,
+              output : 2
+      ]
 
-  final config
-  MLPClassifierLinear(){
-    config = new NeuralNetConfiguration.Builder().
-                 seed(123).
-                 iterations(2).
-                 optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).
-                 learningRate(0.01d).
-                 updater(Updater.NESTEROVS).
-                 momentum(0.9d).
-                 list().
-                 layer(0,
-                   new DenseLayerBuilder().
-                       nIn(layer.input).
-                       nOut(layer.hidden).
-                       weightInit(WeightInit.XAVIER).
-                       activation(Activation.RELU).
-                       build()
-                 ).
-                 layer(1,
-                   new OutputLayerBuilder(LossFunction.NEGATIVELOGLIKELIHOOD).
-                       weightInit(WeightInit.XAVIER).
-                       activation(Activation.SOFTMAX).
-                       weightInit(WeightInit.XAVIER).
-                       nIn(layer.hidden).
-                       nOut(layer.output).
-                       build()
-                 ).
-                 pretrain(false).
-                 backprop(true).
-                 build()
-  }
+      static class DenseLayerBuilder extends DenseLayer.Builder{}
 
-  static createDataSet(String fileName,batchSize) {
-    new RecordReaderDataSetIterator({
-      def rr = new CSVRecordReader()
-      rr.initialize(new FileSplit(new File(fileName)))
-      rr
-    }(),batchSize,0,2)
-  }
+      static class OutputLayerBuilder extends OutputLayer.Builder{
+          OutputLayerBuilder(org.nd4j.linalg.lossfunctions.ILossFunction lossFunction) {super(lossFunction)}
+          OutputLayerBuilder(org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction lossFunction) {super(lossFunction)}
+      }
 
-  def train(example,int epochNum) {
-    def model = new MultiLayerNetwork(config)
-    model.init()
-    model.setListeners(new ScoreIterationListener(10))
-    
-    epochNum.times {
-      model.fit(example)
-    }
-    model
-  }
+      final def config
 
-  def evalModel(MultiLayerNetwork trainedModel,unknowns) {
-    def eval = new Evaluation(layer.output)
-    while(unknowns.hasNext()){
-      def unknown = unknowns.next()
-      eval.eval(
-        unknown.labels,
-        trainedModel.output(unknown.featureMatrix,false)
-      )
-    }
-    eval
-  }
+      MLPClassifierLinear(){
+          config = new NeuralNetConfiguration.Builder().
+                  seed(123).
+                  maxNumLineSearchIterations(2).
+                  optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).
+                  updater(new Nesterovs(0.01d, 0.9d)).
+                  list().
+                  layer(0,
+                          new DenseLayerBuilder().
+                                  nIn(layer.input).
+                                  nOut(layer.hidden).
+                                  weightInit(WeightInit.XAVIER).
+                                  activation(Activation.RELU).
+                                  build()
+                  ).
+                  layer(1,
+                          new OutputLayerBuilder(LossFunction.NEGATIVELOGLIKELIHOOD).
+                                  weightInit(WeightInit.XAVIER).
+                                  activation(Activation.SOFTMAX).
+                                  weightInit(WeightInit.XAVIER).
+                                  nIn(layer.hidden).
+                                  nOut(layer.output).
+                                  build()
+                  ).
+                  pretrain(false).
+                  backprop(true).
+                  build()
+      }
+
+      static createDataSet(String fileName,batchSize) {
+          new RecordReaderDataSetIterator({
+              def rr = new CSVRecordReader()
+              rr.initialize(new FileSplit(new File(fileName)))
+              rr
+          }(),batchSize,0,2)
+      }
+
+      def train(example,int epochNum) {
+          def model = new MultiLayerNetwork(config)
+          model.init()
+          model.setListeners(new ScoreIterationListener(10))
+
+          epochNum.times {
+              model.fit(example)
+          }
+          model
+      }
+
+      def evalModel(MultiLayerNetwork trainedModel,unknowns) {
+          def eval = new Evaluation(layer.output)
+          while(unknowns.hasNext()){
+              def unknown = unknowns.next()
+              eval.eval(
+                      unknown.labels,
+                      trainedModel.output(unknown.featureMatrix,false)
+              )
+          }
+          eval
+      }
+
+
 }
